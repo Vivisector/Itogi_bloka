6cd2964ba11c204d4d2b8a240b623d0bee3d30c1
Dmitry Beliakov <hulk295@yandex.ru>
2023-01-28 12:58:00 +0300

    сделан более быстрый вариант с одним циклом (через сборку новой строки)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿string[] m1 = { "abrakadabra", "hi", "paste", "thor", "tor", "234" }; // задали массив на 6 элементов
-int cnt = 0; // счетчик нужных элементов
-//Console.WriteLine(m[m.Length-1]);
+Console.WriteLine("Исходный массив:");
+ShowArr(m1);
 
+/////// вариант решения с двумя циклами
+int cnt = 0; // счетчик нужных элементов для заготовки нового массива
 for (int i = 0; i < m1.Length; i++)
 { if (m1[i].Length <= 3) cnt++; }
 string[] m2 = new string[cnt];
@@ -14,6 +16,15 @@ for (int i = 0; i < m1.Length; i++)
         cnt++;
     }
 }
+///////////// окончание варианта с двумя циклами
+
+// вариант решения с одним циклом (через набор элементов в строку) - предпочтительнее
+string s = String.Empty; //заготовили пустую строку
+for (int i = 0; i < m1.Length; i++)
+{
+    if (m1[i].Length <= 3) s = s + m1[i] + " ";
+}
+string[] ss = s.Split(); // строку превратили в массив
 
 void ShowArr(string[] m)
 {
@@ -21,8 +32,11 @@ void ShowArr(string[] m)
         Console.Write(m[k] + " ");
 }
 
-Console.WriteLine("Исходный массив:");
-ShowArr(m1);
-
 Console.WriteLine("\nНовый массив:");
-ShowArr(m2);
+// ShowArr(m2);
+
+// теперь или показываем полученную строку
+// Console.WriteLine(s);
+// или показываем созданный по строке массив
+ShowArr(ss); 
+
